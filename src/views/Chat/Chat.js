b17bda2bc8f20516b7e40c3a6fefc40cca820b7a
SanthoshSettu11 <61324766+SanthoshSettu11@users.noreply.github.com>
2020-08-29 01:23:06 +0530

    adding WebSocket and Chat

--- src/views/Chat/Chat.js
+++ src/views/Chat/Chat.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
-function Chat() {
-  return <div></div>;
+function Chat({ userId }) {
+  const [input, setInput] = useState("");
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerinfo">
+          <h3>Saravanan</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerright">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">Sow</span>
+          Hello
+          <span className="chat__timestamp">10/12/2020</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={(e) => sendMessage(e)}>Send a Message</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./Home.css";
+import Chat from "../Chat/Chat";
+import WebSocketClient from "../../services/WebSocket";
+import { updateSocket } from "../../store/WebSocket/WebSocketActions";
 
 function Home() {
-  return <div>Home</div>;
+  const [toUserId, setToUserId] = useState(
+    "b48b4120-e490-11ea-a1e9-696efed5dc0b"
+  );
+  const { userId, ws } = useSelector((state) => ({
+    userId: state.loginReducer?.loginData?.userId,
+    ws: state.WebSocketReducer?.ws
+  }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(userId);
+    const ws = new WebSocketClient(
+      "ws",
+      "13.235.135.67",
+      8080,
+      "/newMessenger/letsChat/"
+    );
+    dispatch(updateSocket(ws));
+  }, []);
+
+  return <div>{ws && <Chat userId={toUserId} webSocket={ws}></Chat>}</div>;
 }
 
 export default Home;
